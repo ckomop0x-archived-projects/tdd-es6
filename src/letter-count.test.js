de662223dfe3999f5af98efa93cb6c53c44fc5b9
Pavel Klochkov <paul.klochkov@gmail.com>
2020-04-21 23:20:09 +0200

    TDD start

--- src/letter-count.test.js
+++ src/letter-count.test.js
@@ -2,9 +2,21 @@ import { expect } from "chai";
 import { getLetterCount } from "./letter-count";
 
 describe("getLetterCount - basic functionality", () => {
-  it("returns an empty object when passed an empty string", () => {
+  it("return an empty object when passed an empty string", () => {
     const expected = {};
     const actual = getLetterCount("");
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it("should return the correct letter count for a word with only one of each letter", () => {
+    const expected = { c: 1, a: 1, t: 1 };
+    const actual = getLetterCount("cat");
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it("should return the correct letter count for a word with more than one of each letter", () => {
+    const expected = { p: 3, u: 1, y: 1 };
+    const actual = getLetterCount("puppy");
     expect(actual).to.deep.equal(expected);
   });
 });
