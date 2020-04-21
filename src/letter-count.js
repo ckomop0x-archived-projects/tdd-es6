6cac2598982c923e516e543ac62184f37c5f6cd5
Pavel Klochkov <paul.klochkov@gmail.com>
2020-04-21 23:23:24 +0200

    Update code

--- src/letter-count.js
+++ src/letter-count.js
@@ -1,14 +1,8 @@
 export const getLetterCount = (string) => {
   const letters = string.split('');
-  let letterCount = {};
+  const letterCount = {};
 
-  letters.forEach(letter => {
-    if (!letterCount[letter]) {
-      letterCount[letter] = 1;
-    } else {
-      letterCount[letter] = letterCount[letter] + 1;
-    }
-  })
+  letters.forEach(letter => !letterCount[letter] ? letterCount[letter] = 1 : letterCount[letter] += 1)
 
   return letterCount;
 };
