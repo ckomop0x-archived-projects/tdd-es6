88018cf380f20a1589da8e515fc665cd30e9ef26
Pavel Klochkov <paul.klochkov@gmail.com>
2020-05-05 11:23:45 +0200

    Update tests

--- src/anagrams.test.js
+++ src/anagrams.test.js
@@ -3,26 +3,26 @@ import { isAnagrams } from "./isAnagrams";
 
 describe("anagrams TDD challenge", () => {
   it('should pass anagram for "listen" and "silent"', () => {
-    expect(isAnagrams("listen", "silent")).to.deep.equal(true);
+    expect(isAnagrams("listen", "silent")).to.equal(true);
   });
 
   it('should pass anagram for "elbow" and "below"', () => {
-    expect(isAnagrams("elbow", "below")).to.deep.equal(true);
+    expect(isAnagrams("elbow", "below")).to.equal(true);
   });
 
   it('shouldn\'t pass anagram for "elbows" and "below"', () => {
-    expect(isAnagrams("elbows", "below")).to.deep.equal(false);
+    expect(isAnagrams("elbows", "below")).to.equal(false);
   });
 
   it('shouldn\'t pass anagram for "listens" and "silent"', () => {
-    expect(isAnagrams("listens", "silent")).to.deep.equal(false);
+    expect(isAnagrams("listens", "silent")).to.equal(false);
   });
 
   it('should pass anagram for "conversation" and "voices rant on"', () => {
-    expect(isAnagrams("conversation", "voices rant on")).to.deep.equal(true);
+    expect(isAnagrams("conversation", "voices rant on")).to.equal(true);
   });
 
   it('should pass anagram for "STATE" and "taste"', () => {
-    expect(isAnagrams("STATE", "taste")).to.deep.equal(true);
+    expect(isAnagrams("STATE", "taste")).to.equal(true);
   });
 });
